b512369084332462cee94f8aae06fda6516c89b6
Alejandro Ramirez <soyalejoramirez@gmail.com>
2017-10-11 16:27:05 -0500

    Ponemos a fallar la prueba unitaria

--- proyecto1/src/test/java/co/com/saimyr/service/NotifyPersonServiceTest.java
+++ proyecto1/src/test/java/co/com/saimyr/service/NotifyPersonServiceTest.java
@@ -27,6 +27,6 @@ public class NotifyPersonServiceTest {
 		//Act
 		String message = notifyPersonService.notify(person);
 		//Assert
-		Assert.assertNotNull(message);
+		Assert.assertEquals("NO Funciona", message);
 	}
 }
